01a536bb06d3e01a136a265178877bce60cc47cd
thachhn <huynhngthach@gmail.com>
2023-12-23 10:16:20 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Thach Huynh on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
